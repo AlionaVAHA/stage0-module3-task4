170cbc7b9e277119709cb2a42bd1f60e09bca479
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-14 23:25:01 -0400

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+       byte first = 9;
+        char  a = 'a';
+        byte degree1 = 2;
+        byte second = 5;
+        char  b = 'b';
+        byte third = 2;
+        byte forth = 7;
+        byte fifth = 4;
+        System.out.println("("+first+a+"^"+degree1+"-"+second+b
+                +"+"+third+"+"+a+"-"+forth+")"
+                +"*"+"("+"("+a+"+"+b+"-"+fifth+a+b+")"+
+                "/"+degree1+")");
     }
 }
